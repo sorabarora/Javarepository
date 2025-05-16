2c05f575632f65dbe1b53ef6d2874440d2dafb1d
Asus <Asus@LAPTOP-915NAFGL>
2025-05-17 00:05:50 +0530

    This is the second commit.

---
+++ Java_Pratice_Sessions/src/jps/Student_1.java
@@ -0,0 +1,11 @@
+package jps;
+
+public class Student_1 {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to all of you");
+		System.out.println("Automation is very easy");
+	}
+
+}
